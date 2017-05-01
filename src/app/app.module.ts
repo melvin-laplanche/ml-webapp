a810b898029e6525a2932071888404fcc92786b7
Melvin Laplanche <Nivl@users.noreply.github.com>
2017-04-30 17:03:53 -0700

    feat(app): Add a basic header with an empty sidebar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,27 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
-import { AppComponent } from './app.component';
-
+// Angular material
 import 'hammerjs';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MdToolbarModule, MdSidenavModule } from '@angular/material';
+import { MdButtonModule } from '@angular/material';
+
+// App components
+import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    BrowserAnimationsModule,
+    MdToolbarModule,
+    MdSidenavModule,
+    MdButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
