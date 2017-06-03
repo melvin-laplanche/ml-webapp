7c18da71336253711ca0ec0be2d454eab96cd87e
Melvin Laplanche <Nivl@users.noreply.github.com>
2017-06-02 20:44:19 -0700

    test: Add tests for HeaderComponent

--- src/app/shared/header/header.component.spec.ts
+++ src/app/shared/header/header.component.spec.ts
@@ -1,6 +1,12 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 
 import { HeaderComponent } from './header.component';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MdToolbarModule, MdButtonModule } from '@angular/material';
+import { MdInputModule, MdIconModule } from '@angular/material';
+import { ReactiveFormsModule } from '@angular/forms';
 
 describe('HeaderComponent', () => {
   let component: HeaderComponent;
@@ -8,9 +14,17 @@ describe('HeaderComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ HeaderComponent ]
+      declarations: [HeaderComponent],
+      imports: [
+        BrowserAnimationsModule,
+        MdButtonModule,
+        MdToolbarModule,
+        MdInputModule,
+        MdIconModule,
+        ReactiveFormsModule,
+      ],
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
@@ -19,7 +33,38 @@ describe('HeaderComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should be created', () => {
-    expect(component).toBeTruthy();
+  it('should have a search bar', () => {
+    component.hasSearchBar = true;
+    fixture.detectChanges();
+
+    const el = fixture.debugElement.query(By.css('.desktop-search-bar'));
+    expect(el).toBeTruthy();
+  });
+
+  it('should NOT have a search bar', () => {
+    component.hasSearchBar = false;
+    fixture.detectChanges();
+
+    const el = fixture.debugElement.query(By.css('.desktop-search-bar'));
+    expect(el).toBeFalsy();
+  });
+
+  it('should raise onMenuOpen event when clicked', () => {
+    let menuIsOpen: boolean = false;
+    component.onMenuOpen.subscribe(() => menuIsOpen = true);
+
+    const menu = fixture.debugElement.query(By.css('.qa-menu-button'));
+    menu.triggerEventHandler('click', null);
+    expect(menuIsOpen).toBe(true);
+  });
+
+  it('should raise onSearchEvent event when a search occurs', () => {
+    let searchData: string = "";
+    component.onSearchEvent.subscribe((data: string) => searchData = data);
+
+    const expectedData = "search query"
+    component.searchForm.controls['query'].setValue(expectedData);
+    component.onSearch()
+    expect(searchData).toBe(expectedData);
   });
 });
