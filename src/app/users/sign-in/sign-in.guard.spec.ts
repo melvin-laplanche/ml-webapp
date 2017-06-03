a89ec473b4749429490ebfe266e93731c7f41902
Melvin Laplanche <Nivl@users.noreply.github.com>
2017-06-03 14:54:29 -0700

    tests: Add test for SignUpGuard and SignInGuard

--- src/app/users/sign-in/sign-in.guard.spec.ts
+++ src/app/users/sign-in/sign-in.guard.spec.ts
@@ -1,15 +1,38 @@
 import { TestBed, async, inject } from '@angular/core/testing';
+import { Observable } from 'rxjs/Observable';
+
+import { Store } from '@ngrx/store';
+class StoreStub {
+  select(state: string): Observable<boolean> {
+    return Observable.of(false);
+  }
+}
 
 import { SignInGuard } from './sign-in.guard';
 
 describe('SignInGuard', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [SignInGuard]
+      providers: [
+        SignInGuard,
+        { provide: Store, useClass: StoreStub },
+      ]
     });
   });
 
-  it('should ...', inject([SignInGuard], (guard: SignInGuard) => {
-    expect(guard).toBeTruthy();
+  it('Should NOT active when the user is logged in', inject([SignInGuard, Store], (guard: SignInGuard, store: Store<any>) => {
+    const spy = spyOn(store, 'select').and.returnValue(Observable.of(true));
+
+    (<Observable<boolean>>guard.canActivate(null, null)).subscribe((isActive: boolean) => {
+      expect(isActive).toBeFalsy();
+    })
+  }));
+
+  it('Should active when the user is NOT logged in', inject([SignInGuard, Store], (guard: SignInGuard, store: Store<any>) => {
+    const spy = spyOn(store, 'select').and.returnValue(Observable.of(false));
+
+    (<Observable<boolean>>guard.canActivate(null, null)).subscribe((isActive: boolean) => {
+      expect(isActive).toBeTruthy();
+    })
   }));
 });
--- src/app/users/sign-up/sign-up.guard.spec.ts
+++ src/app/users/sign-up/sign-up.guard.spec.ts
@@ -1,15 +1,38 @@
 import { TestBed, async, inject } from '@angular/core/testing';
+import { Observable } from 'rxjs/Observable';
+
+import { Store } from '@ngrx/store';
+class StoreStub {
+  select(state: string): Observable<boolean> {
+    return Observable.of(false);
+  }
+}
 
 import { SignUpGuard } from './sign-up.guard';
 
 describe('SignUpGuard', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [SignUpGuard]
+      providers: [
+        SignUpGuard,
+        { provide: Store, useClass: StoreStub },
+      ]
     });
   });
 
-  it('should ...', inject([SignUpGuard], (guard: SignUpGuard) => {
-    expect(guard).toBeTruthy();
+  it('Should NOT active when the user is logged in', inject([SignUpGuard, Store], (guard: SignUpGuard, store: Store<any>) => {
+    const spy = spyOn(store, 'select').and.returnValue(Observable.of(true));
+
+    (<Observable<boolean>>guard.canActivate(null, null)).subscribe((isActive: boolean) => {
+      expect(isActive).toBeFalsy();
+    })
+  }));
+
+  it('Should active when the user is NOT logged in', inject([SignUpGuard, Store], (guard: SignUpGuard, store: Store<any>) => {
+    const spy = spyOn(store, 'select').and.returnValue(Observable.of(false));
+
+    (<Observable<boolean>>guard.canActivate(null, null)).subscribe((isActive: boolean) => {
+      expect(isActive).toBeTruthy();
+    })
   }));
 });
