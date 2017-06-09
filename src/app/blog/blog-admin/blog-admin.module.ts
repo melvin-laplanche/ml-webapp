c52a119223168982259629344f6fc24b972b54c4
Melvin Laplanche <Nivl@users.noreply.github.com>
2017-06-08 22:43:53 -0700

    feat(blog): Create empty BlogAdminComponent

--- src/app/blog/blog-admin/blog-admin.module.ts
+++ src/app/blog/blog-admin/blog-admin.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { BlogAdminComponent } from './blog-admin.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: []
+  declarations: [BlogAdminComponent]
 })
 export class BlogAdminModule { }
