29258706201db71b78e0f4b09695765af234b7df
Melvin Laplanche <Nivl@users.noreply.github.com>
2017-06-09 23:22:49 -0700

    feat: Add user.is_admin

--- src/app/users/users.model.ts
+++ src/app/users/users.model.ts
@@ -2,12 +2,20 @@ export class UserPayload {
   public id = '';
   public name = '';
   public email = '';
+  public is_admin?= false;
 }
-
 
 export class User extends UserPayload {
   constructor(payload: UserPayload) {
     super();
     Object.assign(this, payload);
+  }
+
+  get isAdmin(): boolean {
+    return this.is_admin;
+  }
+
+  set isAdmin(value: boolean) {
+    this.is_admin = value;
   }
 }
